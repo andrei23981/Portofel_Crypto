07858a0a8679c699dfb23273b6edfa78d24708d2
andrei23981 <andrei.chindris05@e-uvt.ro>
2025-05-26 14:37:26 +0000

    hh

--- crypto-wallet-qt/main.cpp
+++ crypto-wallet-qt/main.cpp
@@ -1,1 +1,10 @@
+#include <QApplication>
+#include "mainwindow.h"
+
 // Main entry point for the crypto wallet application
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    MainWindow w;
+    w.show();
+    return app.exec();
+}
