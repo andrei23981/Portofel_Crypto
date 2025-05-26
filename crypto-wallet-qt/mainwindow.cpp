8eb802aa4cc195e850088861ac6973ec758032e3
andrei23981 <andrei.chindris05@e-uvt.ro>
2025-05-26 12:18:35 +0000

    gg

--- crypto-wallet-qt/mainwindow.cpp
+++ crypto-wallet-qt/mainwindow.cpp
@@ -1,1 +1,42 @@
 // MainWindow implementation
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "api.h"
+#include "utils.h"
+
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent), ui(new Ui::MainWindow){
+    ui->setupUi(this);
+    ui->comboBoxMoneda->addItems({"bitcoin", "ethereum", "solana"});
+    actualizeazaPret();
+}
+MainWindoW::~MainWindow(){
+    delete ui;
+}
+void MainWindoW::on_buttonCumpara_clicked() {
+    QString sumaText = ui->lineEditSuma->text();
+    if (!isValidSuma(sumaText)) {
+        QMessageBox::warning(this, "Eroare", "Introduceți suma de cumpărat.");
+        return;
+    }
+    QString simbol = ui->comboBoxMoneda->currentText();
+    QString pretStr = API::getPret(simbol);
+    if (pretStr.isEmpty()) {
+        QMessageBox::warning(this, "Eroare", "Nu s-a putut obține prețul pentru moneda selectată.");
+        return;
+    }
+
+    QMessageBox::information(this, "Cumparare reusita", "Ai cumparat cu succes "+simbol.toUpper() + " de " + sumaText + " RON.");
+
+    ui->lineEditSuma->clear();
+}
+void MainWindow::actualizeazaPret() {
+    QString simbol = ui->comboBoxMoneda->currentText();
+    QString pretStr = API::getPret(simbol);
+    if (!pretStr.isEmpty())
+        ui->labelPretCurent->setText("Preț curent: " + pretStr + " RON");
+    else
+        ui->labelPretCurent->setText("Preț indisponibil");
+}
--- crypto-wallet-qt/mainwindow.h
+++ crypto-wallet-qt/mainwindow.h
@@ -1,1 +1,25 @@
 // MainWindow header file
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+    class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindoW: public QMainWindow{
+    Q_OBJECT
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+private slots:
+    void on_buttonCumpara_clicked();
+    void on_comboBoxMoneda_currentIndexChanged(int index);
+private:
+    Ui::MainWindow *ui;
+    void actualizeazaPret();
+}
+
+#endif // MAINWINDOW_H
